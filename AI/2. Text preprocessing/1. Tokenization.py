7c904bdd2f903541e19fc3654966c31158ff77ca
devLupin <luckyquit49@gmail.com>
2021-02-02 15:30:30 +0900

    for git pull

--- AI/2. Text preprocessing/1. Tokenization.py
+++ AI/2. Text preprocessing/1. Tokenization.py
@@ -26,3 +26,7 @@ import kss
 
 text='딥 러닝 자연어 처리가 재미있기는 합니다. 그런데 문제는 영어보다 한국어로 할 때 너무 어려워요. 농담아니에요. 이제 해보면 알걸요?'
 print(kss.split_sentences(text))
+
+
+# https://www.grammarly.com/blog/engineering/how-to-split-sentences/
+# 햬당 링크는 문장 토큰화 규칙을 짤 때 발생할 수 있는 예외사항을 다룬 참고자료임.
