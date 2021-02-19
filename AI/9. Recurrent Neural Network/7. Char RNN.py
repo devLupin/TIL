0abc8e752b044c887a86d2daaadb55a87d2f2159
devLupin <luckyquit49@gmail.com>
2021-02-19 17:04:25 +0900

    TIL Word2Vec, Negative Sampling

--- AI/9. Recurrent Neural Network/7. Char RNN.py
+++ AI/9. Recurrent Neural Network/7. Char RNN.py
@@ -88,3 +88,108 @@ def sentence_generation(model, length):
     return ('').join(y_char)
 
 sentence_generation(model, 100)
+
+
+
+""" many-to-one 구조의 RNN 글자 단위 학습 및 텍스트 생성 """
+# %%
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+
+text='''
+I get on with life as a programmer,
+I like to contemplate beer.
+But when I start to daydream,
+My mind turns straight to wine.
+
+Do I love wine more than beer?
+
+I like to use words about beer.
+But when I stop my talking,
+My mind turns straight to wine.
+
+I hate bugs and errors.
+But I just think back to wine,
+And I'm happy once again.
+
+I like to hang out with programming and deep learning.
+But when left alone,
+My mind turns straight to wine.
+'''
+# %%
+tokens = text.split()   # '\n' 제거
+text = ' '.join(tokens)
+print(text)
+# %%
+char_vocab = sorted(list(set(text)))
+print(char_vocab)
+# %%
+vocab_size = len(char_vocab)
+print(vocab_size)   # 33
+# %%
+char_to_index = dict((c, i) for i, c in enumerate(char_vocab))  # integer index
+print(char_to_index)
+# %%
+length = 11
+sequences = []
+
+for i in range(length, len(text)) :
+    seq = text[i-length:i]  # 길이 11의 문자열을 지속적으로 만든다.
+    sequences.append(seq)
+
+print(len(sequences))   # 426
+# %%
+sequences[:10]
+# %%
+sequences[:45]
+# %%
+X = []
+for line in sequences:
+    temp_X = [char_to_index[char] for char in line]
+    X.append(temp_X)
+# %%
+""" 분리작업 """
+sequences = np.array(X)
+X = sequences[:, :-1]
+y = sequences[:, -1]
+# %%
+""" one-hot encoding """
+sequences = [to_categorical(x, num_classes=vocab_size) for x in X]  # one-hot encoding for X
+X = np.array(sequences)
+y = to_categorical(y, num_classes=vocab_size)   # one-hot encoding for 
+
+print(X.shape)
+# %%
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+model = Sequential()
+model.add(LSTM(80, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dense(vocab_size, activation='softmax'))
+# %%
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=100, verbose=2)
+# %%
+# args : 모델, 인덱스 정보, 문장 길이, 초기 시퀸스, 반복 횟수
+def sentence_generation(model, char_to_index, seq_length, seed_text, n) :
+    init_text = seed_text
+    sentence = ''
+    
+    for _ in range(n) :
+        encoded = [char_to_index[char] for char in seed_text]   # integer encoding
+        encoded = pad_sequences([encoded], maxlen=seq_length, padding='pre')
+        encoded = to_categorical(encoded, num_classes=len(char_to_index))
+        result = model.predict_classes(encoded, verbose=0)
+        
+        for char, index in char_to_index.items() :
+            if index == result :    # 예측 글자면?
+                break
+        
+        seed_text = seed_text + char
+        sentence = sentence + char
+        
+    sentence = init_text + sentence
+    return sentence
+# %%
+print(sentence_generation(model, char_to_index, 10, 'I get on w', 80))
